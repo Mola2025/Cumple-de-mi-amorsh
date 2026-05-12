5b3f5a249cc1d7762bfc3c76ccb5041605a6bab7
David Molano <davidm0l4n0@gmail.com>
2026-05-11 23:28:41 -0400

    Modificaciones para poder hacer deploy con Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,13 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
 // @cloudflare/vite-plugin builds from this — wrangler.jsonc main alone is insufficient.
 export default defineConfig({
+  vite: {
+    // Esto es VITAL para que los links no se rompan en GitHub Pages
+    base: "/Cumple-de-mi-amorsh/",
+    build: {
+      outDir: "dist",
+    },
+  },
   tanstackStart: {
     server: { entry: "server" },
   },
